2098823e60acb4fb80f4479686f471e735ae2391
Eduard38655 <93397077+Eduard38655@users.noreply.github.com>
2023-07-05 22:34:26 -0400

    Add files via upload
    Primer Comentario.

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+function features(params) {
+    document.getElementById("myDropdown-features").classList.toggle("myDropdown-features-style");
+document.getElementById("myDropdown-features").classList.toggle("show-features");
+ 
+}
+ 
+
+
+
+function company(params) {
+    document.getElementById("myDropdown-company").classList.toggle("show-company");
+  document.getElementById("myDropdown-company").classList.toggle("myDropdown-company-style");
+}
+
+function hamburguer_menu(params) {
+   document.getElementById("dropdown-features").classList.toggle( "hamburguer_menu-show-features");
+   document.getElementById("dropdown-company").classList.toggle("hamburguer_menu-show-company");
+   document.getElementById("save-info").classList.toggle("cover");
+
+}
+
+
+
+
+
+
+ 
